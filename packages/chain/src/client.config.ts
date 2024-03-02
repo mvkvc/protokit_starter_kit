96dc8a3f18c075634600f1c55e11f194c6d0e1eb
Marko Vukovic <mvkvc.git@gmail.com>
2024-03-01 21:55:30 -0500

    added deploys

--- packages/chain/src/client.config.ts
+++ packages/chain/src/client.config.ts
@@ -1,16 +1,33 @@
 import { ClientAppChain } from "@proto-kit/sdk";
+import { Field } from "o1js";
 import runtime from "./runtime";
+import { hashSha, hashPoseidon } from "./utils";
+// import { preimageProgram, preimageProof } from "./completions";
+
+// Everything needed for instantiating a ClientAppChain and easily submitting transactions should be here
 
 const appChain = ClientAppChain.fromRuntime(runtime);
 
-const graphqlHost = process.env.CHAIN_GRAPHQL_HOST || "0.0.0.0";
-const graphqlPORT = process.env.CHAIN_GRAPHQL_PORT || "8080";
-const graphqlURL = `http://${graphqlHost}:${graphqlPORT}/graphql`;
+export function getClient(graphqlURL: string | undefined = undefined) {
+  appChain.configurePartial({
+    GraphqlClient: {
+      url: graphqlURL || 'https://replicant-chain.fly.dev',
+    },
+  });
+}
 
-appChain.configurePartial({
-  GraphqlClient: {
-    url: graphqlURL,
-  },
-});
+export function hashData(data: string | object): Field {
+  let inputString;
+  if (typeof data === 'string') {
+    inputString = data;
+  } else if (typeof data === 'object') {
+    inputString = JSON.stringify(data);
+  } else {
+    throw new Error('Invalid data type');
+  }
 
-export const client = appChain;
+  const hashedSha = hashSha(inputString);
+  return hashPoseidon(hashedSha)
+}
+
+// Function to generate proof
--- packages/chain/src/utils.ts
+++ packages/chain/src/utils.ts
@@ -1,5 +1,8 @@
 import { Option, assert } from "@proto-kit/protocol";
+import { CircuitString, Field, Poseidon } from "o1js";
+import * as crypto from "crypto";
 
+// Check if isSome and throw with assert and message if no default otherwise set
 export function unwrap<T>(
   option: Option<T>,
   message: string,
@@ -14,4 +17,16 @@ export function unwrap<T>(
   let result: T = default_ !== undefined ? default_ : option.value;
 
   return result;
+}
+
+export function hashSha(input: string): string {
+  return crypto.createHash("sha256")
+    .update(input)
+    .digest("hex");
+}
+
+export function hashPoseidon(input: string): Field {
+  const inputCS = CircuitString.fromString(input);
+  const inputFields = inputCS.toFields();
+  return Poseidon.hash(inputFields);
 }
